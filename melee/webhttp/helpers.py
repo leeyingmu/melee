7ee984f2805a28316ff13c37ded28df176cb9628
FaChang <fachang.li@ihandysoft.com>
2015-07-30 14:37:00 +0800

    扩展melee, 增加更多自动配置项

--- melee/webhttp/helpers.py
+++ melee/webhttp/helpers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import hmac, hashlib
+import hmac, hashlib, time
 from melee.core import json
 
 def format_ok_response(data=None):
@@ -8,18 +8,22 @@ def format_ok_response(data=None):
     return jsonify(meta={'code': 200, 'message': 'ok'}, data=data) if data is not None else jsonify(meta={'code': 200, 'message': 'ok'})
 
 def send_test_request(client, url, content, sig_kv=None, sig_key=None, files=None, post=True):
-    sig = hmac.new(sig_key, json.dumps(content), hashlib.sha256).hexdigest()
+    t = int(time.time()*1000)
+    content = json.dumps(content)
+    rawdata = '%s%s' % (content, t)
+    sig = hmac.new(sig_key, rawdata, hashlib.sha256).hexdigest()
     data = {
-        'content': json.dumps(content),
+        'content': content,
+        'timestamp': t,
         'signature': sig,
         'sig_kv': sig_kv
     }
     if post:
         if files:
-            content.update(files)
+            data.update(files)
         rs = client.post(url, data=data)
     else:
-        query_string = '&'.join(['%s=%s' % (k,v) for k,v in content.iteritems()])
+        query_string = '&'.join(['%s=%s' % (k,v) for k,v in data.iteritems()])
         rs =  client.get(url, query_string=query_string)
     return rs.status_code, rs.data
 
